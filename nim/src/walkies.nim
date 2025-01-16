b1f264baa2727e08f926ddb8540620ffd80eb2fa
J0eCool <count.j0ecool@gmail.com>
2025-01-16 09:58:02 -0800

    Move the face, clamp it within bounds

--- nim/src/walkies.nim
+++ nim/src/walkies.nim
@@ -6,22 +6,36 @@ import gdext/classes/[
 ]
 
 type Walkies* = ptr object of Node2D
+  spd* {.gdexport.}: float
+  boundLo* {.gdexport.}: float
+  boundHi* {.gdexport.}: float
+
+  player: Node2D
   anim: AnimationPlayer
-  #types defined here,for instance label_node:Label if class were different node type
+  dx: int
 
 method ready(self: Walkies) {.gdsync.} =
-  #running code in the editor may produce unintented scene changes, even if nodes are hidden
   if Engine.isEditorHint: return
+  self.player = self.getNode("Player") as Node2D
   self.anim = self.getNode("Player/AnimationPlayer") as AnimationPlayer
+
+method process(self: Walkies, dt: float) {.gdsync.} =
+  if Engine.isEditorHint: return
+  case self.dx
+  of -1: self.anim.play("walk_left")
+  of +1: self.anim.play("walk_right")
+  else: self.anim.play("RESET")
+
+  var p = self.player.position
+  p.x += dt * self.spd*self.dx
+  p.x = clamp(p.x, self.boundLo, self.boundHi)
+  self.player.position = p
 
 method input(self: Walkies, event: GdRef[InputEvent]) {.gdsync.} =
   if event[].is_action_pressed("left"):
-    print("anim.play('walk_left')")
-    self.anim.play("walk_left")
+    self.dx = -1
   elif event[].is_action_pressed("right"):
-    print("anim.play('walk_right')")
-    self.anim.play("walk_right")
+    self.dx = +1
 
   if event[].isActionReleased("left") or event[].isActionReleased("right"):
-    print("anim.play('RESET')")
-    self.anim.play("RESET")
+    self.dx = 0
