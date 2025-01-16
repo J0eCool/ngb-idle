093e33b76b5a4c83270b4908d42fe24dd5372b71
J0eCool <count.j0ecool@gmail.com>
2025-01-15 17:24:09 -0800

    add some stub classes

--- nim/bootstrap.nim
+++ nim/bootstrap.nim
@@ -2,15 +2,15 @@ import gdext
 
 # import your extension classes here
 import my_class
+import thinger
 
 # ==================================
 
 proc register_classes {.execon: initialize_scene.} =
   # register your extension classes here
   register MyClass
-
-  # ====================================
-  discard
+  register Stinger
+  register Thinger
 
 
 GDExtensionEntryPoint
