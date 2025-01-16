27ed4e311db5a3b69b2e02e8bf689083c8a3c543
J0eCool <count.j0ecool@gmail.com>
2025-01-15 17:59:38 -0800

    reacting to input

--- nim/src/my_class.nim
+++ nim/src/my_class.nim
@@ -1,5 +1,8 @@
 import gdext
-import gdext/classes/[gdLabel]
+import gdext/classes/[
+  gdInputEvent,
+  gdLabel,
+]
 
 type MyClass* = ptr object of Label
   #types defined here,for instance label_node:Label if class were different node type
@@ -10,3 +13,9 @@ method ready(self: MyClass) {.gdsync.} =
 
   #self.label_node.text if class were different node type
   self.text = "Listen up, buttercup"
+
+method input(self: MyClass, event: GdRef[InputEvent]) {.gdsync.} =
+  if event[].is_action_pressed("up"):
+    self.text = "uppies"
+  elif event[].is_action_pressed("down"):
+    self.text = "downer..."
