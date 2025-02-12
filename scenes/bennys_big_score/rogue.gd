7e56a5b7a3ea446b298c57b71a941eea10f4bef8
J0eCool <count.j0ecool@gmail.com>
2025-02-12 13:15:56 -0800

    s/rogue/rogue_player/g

--- scenes/bennys_big_score/rogue.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody3D
-
-@export var top_speed: float = 2.4
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("enter"):
-		$AnimationPlayer.play("1H_Melee_Attack_Chop")
